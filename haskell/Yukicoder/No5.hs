f3d4bdf123c46740965ba9819ba62287b2267ee7
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-05-15 18:31:49 +0900

    solved yuki 5

--- haskell/Yukicoder/No5.hs
+++ haskell/Yukicoder/No5.hs
@@ -1,5 +1,9 @@
 module Main where
 
+-- https://yukicoder.me/problems/no/5
+
+import Data.List (sort)
+
 split :: String -> [String]
 split = go ("", [])
   where
@@ -11,5 +15,17 @@ split = go ("", [])
 prompt :: IO [String]
 prompt = do split <$> getLine
 
+solve :: Int -> Int -> [Int] -> Int
+solve l n ws = go 0 l (sort ws)
+  where
+    go :: Int -> Int -> [Int] -> Int
+    go c acc [] = c
+    go c 0 _ = c
+    go c acc (x : xs) = if acc < x then c else go (c + 1) (acc - x) xs
+
 main :: IO ()
-main = return ()
+main = do
+  l :: Int <- read . head <$> prompt
+  n :: Int <- read . head <$> prompt
+  ws :: [Int] <- map read <$> prompt
+  print $ solve l n ws
